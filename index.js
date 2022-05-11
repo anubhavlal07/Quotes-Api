599237577f017fe08120f49f0d27f6e511ef0b41
ANUBHAV LAL <anubhavlal.15@gmail.com>
2022-05-11 21:16:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,3 +26,33 @@ fetch(url, {
 };
 getQuote();
 btn.addEventListener("click", getQuote);
+
+// Disabled Input from keyboard
+(document.onkeydown = function (event) {
+	if (event.keyCode == 123) {
+	  return false;
+	} else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+	  return false;
+	} else if (event.ctrlKey && event.shiftKey && event.keyCode == 86) {
+	  return false;
+	} else if (event.ctrlKey && event.shiftKey && event.keyCode == 117) {
+	  return false;
+	} else if (event.ctrlKey && event.keyCode == 85) {
+	  return false;
+	}
+ }),
+	false;
+ 
+ if (document.addEventListener) {
+	document.addEventListener(
+	  "contextmenu",
+	  function (e) {
+		 e.preventDefault();
+	  },
+	  false
+	);
+ } else {
+	document.attachEvent("oncontextmenu", function () {
+	  window.event.returnValue = false;
+	});
+ }
